283b8bbc68a5ef2e4321ef9ae3346013615513b2
MadhuMitaSiva <madhumitasiva.official@gmail.com>
2026-08-03 21:11:11 +0530

    Updating task_6

--- Week 3/onboarding-quantization/Madhumita S/task_06_pytorch_to_int8_tflite/convert_model.py
+++ Week 3/onboarding-quantization/Madhumita S/task_06_pytorch_to_int8_tflite/convert_model.py
@@ -55,3 +55,29 @@ except Exception as e:
     traceback.print_exc()
     print("Conversion failed")
 
+
+import tensorflow as tf
+import numpy as np
+import os
+def representative_dataset():
+    for i in range(50):
+        sample = np.load(f".\\onboarding-quantization\\Madhumita S\\task_06_pytorch_to_int8_tflite\\calib\\{i}.npy")
+        # Convert from (1,28,28) to (28,28,1)
+        sample = np.transpose(sample, (1,2,0))
+        # Add batch dimension
+        sample = np.expand_dims(sample, axis=0)
+        yield [sample.astype(np.float32)]
+
+converter = tf.lite.TFLiteConverter.from_saved_model(".\\onboarding-quantization\\Madhumita S\\task_06_pytorch_to_int8_tflite\\tf_model")
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset
+converter.target_spec.supported_ops = [
+    tf.lite.OpsSet.TFLITE_BUILTINS_INT8
+]
+converter.inference_input_type = tf.int8
+converter.inference_output_type = tf.int8
+tflite_model = converter.convert()
+with open(".\\onboarding-quantization\\Madhumita S\\task_06_pytorch_to_int8_tflite\\model_int8.tflite", "wb") as f:
+    f.write(tflite_model)
+
+print("INT8 model saved successfully.")
